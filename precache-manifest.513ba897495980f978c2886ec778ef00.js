0fa7845269a5a04b01fbd5e1b71210cb6bbd695e
Adu Daniel <adudaniel097@gmail.com>
2022-12-01 17:59:32 +0000

    Updates

---
+++ precache-manifest.513ba897495980f978c2886ec778ef00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7b0f31838ddaacefdcf",
+    "url": "/Instagram-clone/static/css/main.dce3e29e.chunk.css"
+  },
+  {
+    "revision": "b7b0f31838ddaacefdcf",
+    "url": "/Instagram-clone/static/js/main.b7b0f318.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/Instagram-clone/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "b2d8e58bd867439fa7b2",
+    "url": "/Instagram-clone/static/js/2.b2d8e58b.chunk.js"
+  },
+  {
+    "revision": "e9f23d5182653b42535a",
+    "url": "/Instagram-clone/static/js/runtime~main.e9f23d51.js"
+  },
+  {
+    "revision": "0491a145e958bf3a3022bb69420121cd",
+    "url": "/Instagram-clone/index.html"
+  }
+];
